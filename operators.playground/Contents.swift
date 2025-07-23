a7321926166c99771b087f09dd2dadbaada44155
SDC-USER <sdc-user@VY-7F-s-045.local>
2025-07-23 15:01:08 +0530

    operators

---
+++ operators.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var myScore = 8 * 4
+print(myScore)
+
+var oppScore:Double = 80/3
+print(oppScore)
+
+var finalScore = Double(myScore) + oppScore
+print("The final score is \(finalScore)")
