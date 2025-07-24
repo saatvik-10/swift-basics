cec4f57c02cf900d8237b00047eed98ed2dd2cb5
SDC-USER <sdc-user@VY-7F-s-045.local>
2025-07-24 10:56:11 +0530

    removed errors

--- Student Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Student Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -8,15 +8,15 @@ var secondDecimal:Float = 0
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var trueOrFalse:Bool
-firstDecimal = trueOrFalse
+//firstDecimal = trueOrFalse
 print("Error will be there coz we cannot assign a BOOL to a FLOAT")
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var test:String = "asd"
-firstDecimal = test
+//firstDecimal = test
 print("Error will be there coz we cannot assign a string to a float")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 var testVal : Int = 0
-firstDecimal = testVal
+//firstDecimal = testVal
 print("will get an error bcz we cant assign a float to the type int")
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
