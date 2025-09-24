e9b17078a599841f2288da50454603d189c9feb4
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-24 16:09:44 +0530

    protocol encoding

--- protocols_practice.playground/Contents.swift
+++ protocols_practice.playground/Contents.swift
@@ -30,3 +30,27 @@ var s2 = Student(rno: 2, fName: "Pavneesh", lName: "Sharma")
 var s3 = Student(rno: 2, fName: "Pavneesh", lName: "Sharma")
 
 print(s3 == s2)
+
+import Foundation
+
+class IOSSDP: Codable {
+    var institute: String
+        var name: String
+        var year: Int
+    
+    init(institute: String, name: String, year: Int) {
+        self.institute = institute
+        self.name = name
+        self.year = year
+    }
+}
+
+var mitSDP = IOSSDP(institute: "mit-wpu", name: "Saatvik", year: 2025)
+
+var jsonEncoder = JSONEncoder();
+
+if let data = try? jsonEncoder.encode(mitSDP) {
+    if let jsonString = String(data: data, encoding: .ascii) {
+        print(jsonString)
+    }
+}
